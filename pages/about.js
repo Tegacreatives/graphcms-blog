fb169128fc27cf073f683fa32e422726282ed254
Tega Okorare <epicboy006@gmail.com>
2022-06-08 21:47:53 +0100

    new changes

--- pages/about.js
+++ pages/about.js
@@ -30,14 +30,14 @@ const about = () => {
               />
           </div>
           <p>The project might not look beautiful or have engaging UI desing but my main focus here<br />
-              wasn't to create a beautiful UI, it's more about intracting with a headless CMS and working with the data from the headless CMS.<br />
+              was not to create a beautiful UI, it is more about intracting with a headless CMS and working with the data from the headless CMS.<br />
               Real Estate blog is a dummy project I created to learn how GraphCMS works using Graphql to query and request data. <br />
               GraphCMS is a headless CMS based on graphGL, it allows you create schemas and models which you can use in defining the<br />
               kind of data you need to work with any frontend framework.<br />
               GraphCMS removes the traditional content management pain points by using the power of GraphQL to make API requests<br />
               more efficient.<br />
               The blog posts you saw on the home page are dynamically fetched from GraphCMS.<br />
-              Unlike other CMS, GraphCMS is automatically hosted so you won't need to worry about deploying it.
+              Unlike other CMS, GraphCMS is automatically hosted so no need to worry about deploying it.
           </p>
     </div>
   )
